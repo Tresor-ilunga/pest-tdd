599e3b52fa6b96f2702480d249b65b2419cff6a7
Tresor-ilunga <19im065@esisalama.org>
2023-08-16 17:15:43 +0200

    Initial commit

---
+++ tests/ApiTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Abstract class ApiTestCase
+ *
+ * @author tresor-ilunga <ilungat82@gmail.com>
+ */
+abstract class ApiTestCase extends BaseTestCase
+{
+    //
+}
