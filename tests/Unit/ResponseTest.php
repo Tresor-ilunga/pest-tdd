58216fdaf4ceb72ec1dd4634b4918846b70748db
Tresor-ilunga <19im065@esisalama.org>
2023-08-16 21:34:59 +0200

    Create ResponseTest

---
+++ tests/Unit/ResponseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+test('a Response object can be created', function () {
+
+    // ACT
+    $response = new Response('{"foo": "bar"}', 200);
+
+
+    // ASSERT
+    expect($response->getStatus())->toBeInt()->toBe(200)
+        ->and($response->getBody())
+        ->toMatchJson(['foo' => 'bar']);
+});
