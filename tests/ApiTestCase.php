c06be79bd393f95cb0e3dc9c761a2482609c0f03
Tresor-ilunga <19im065@esisalama.org>
2023-08-16 21:22:26 +0200

    Return a real response from the json test helper

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
@@ -11,5 +13,11 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
  */
 abstract class ApiTestCase extends BaseTestCase
 {
-    //
+    public function json(string $method = 'GET', string $uri = '/', array $data = [], array $headers = []): Response
+    {
+        $body = '{"id":1,"title":"Clean Code: A Handbook of Agile Software Craftsmanship","year_published":2008,
+        "author":{"id":1,"name":"Robert C. Martin","bio":"This is an author"}}';
+
+        return new Response(body: $body, statusCode: 200, headers: []);
+    }
 }
--- vendor/Expectation.php
+++ vendor/Expectation.php
@@ -4,5 +4,6 @@ namespace Pest;
 
 /**
  * @method \Pest\Expectation toBeOne()
+ * @method \Pest\Expectation toMatchJson(array $expected)
  */
 final class Expectation {}
