37e0928aebc98aa9de17df198a374d7533be81a7
Tresor-ilunga <19im065@esisalama.org>
2023-08-18 20:34:20 +0200

    Add getPath()

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -13,19 +13,28 @@ class Request
 {
     public function __construct(
         private array $queryParams, // $_GET
+        private array $serverVars = []
     )
     {}
 
-    public static function create(string $method, string $uri, iterable $server, string $content): self
+    public static function create(string $method, string $uri, array $server, string $content): self
     {
         $uriParts = parse_url($uri);
         parse_str($uriParts['query'] ?? '', $queryParams);
 
-        return new self($queryParams);
+        $_SERVER['REQUEST_URI'] = $uri;
+        $serverVars             = array_merge($server, $_SERVER);
+
+        return new self($queryParams, $serverVars);
     }
 
     public function getQueryParams(): array
     {
         return $this->queryParams;
+    }
+
+    public function getPath(): string
+    {
+        return strtok($this->serverVars['REQUEST_URI'], '?');
     }
 }
