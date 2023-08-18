9c34b3d7b0c63b2de25e1726ace59c3bab577ec1
Tresor-ilunga <19im065@esisalama.org>
2023-08-18 19:34:11 +0200

    Work out application sequence

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -16,6 +16,20 @@ abstract class ApiTestCase extends BaseTestCase
 {
     public function json(string $method = 'GET', string $uri = '/', array $data = [], array $headers = []): Response
     {
+
+        // Json encode the data
+
+        // Merge server variables with $headers
+
+        // Create a $request using a static named constructor
+
+        // Create / resolve the Kernel
+
+        // Obtain a $response object: $response = $kernel->handle($request)
+
+        // return the $response
+        
+
         $body = '{"id":1,"title":"Clean Code: A Handbook of Agile Software Craftsmanship","year_published":2008,
         "author":{"id":1,"name":"Robert C. Martin","bio":"This is an author"}}';
 
